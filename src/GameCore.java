40402c50bec64a85c9edc7a53da53a1d457094fd
ocramgithub <marstrabalhar@gmail.com>
2023-10-07 22:10:02 +0100

    fixed some bugs and logic problems

--- src/GameCore.java
+++ src/GameCore.java
@@ -14,7 +14,7 @@ public class GameCore {
 
 
     public GameCore() {
-        player = new Player("Marco");
+        player = new Player("Marco", this);
         zombie = new Monster("Zombie", 1200);
         slime = new Monster("Slime", 2100);
         wither = new Monster("Wither", 4900);
@@ -71,9 +71,8 @@ public class GameCore {
                         menuShop();
                     case 8:
                         playerIsDead = true;
-                        break;
                     case 0:
-                        player.xp = 10000;
+                        player.xp = 1000;
                         player.coins = 1000;
                 }
 
@@ -86,29 +85,102 @@ public class GameCore {
             case 1:
                 shop.buyXPMultiplier(player, 100);
                 start();
+                break;
             case 2:
                 shop.buyLife(player, 50);
                 start();
+                break;
             case 3:
                 shop.buyCoins(player);
                 start();
+                break;
             case 4:
                 shop.buySword(player, 75);
                 start();
+                break;
             case 5:
                 shop.sellSword(player);
                 start();
+                break;
             case 6:
                 start();
+                break;
             default:
                 System.out.println("Número inválido.");
                 start();
+                break;
         }
     }
 
     public void checkIfPlayerIsDead(Player player) {
         if (player.hp <= 0) {
             playerIsDead = true;
+        }
+    }
+
+    public void checkAttack(Monster monster) {
+        System.out.println("Você atacou a " + monster.getName());
+        if(monster.hp <= 0) {
+            System.out.println("======================");
+            System.out.println("Este monstro já está morto.");
+            System.out.println("======================");
+        } else {
+            player.random();
+            if(monster.hp <= 80) {
+                System.out.println("======================");
+                if(player.haveSword == true) {
+                    System.out.println("Você usou a sua espada de esmeralda.");
+                    System.out.println("Tirou: " + monster.hp*2.30);
+                    monster.setHp((int) (monster.hp-monster.hp*2.30));
+                } else {
+                    System.out.println("Tirou: " + monster.hp);
+                }
+                monster.setHp(monster.hp-monster.hp);
+                System.out.println("Você matou o " + monster.name);
+                if(monster.name == "Zombie") {
+                    player.xp += 250;
+                    player.coins += 125;
+                    System.out.println("Você recebeu 250 de XP e 125 moedas por derrotar o Zombie.");
+                } else if(monster.name == "Slime") {
+                    player.xp += 500;
+                    player.coins += 250;
+                    System.out.println("Você recebeu 500 de XP e 250 moedas por derrotar a Slime.");
+                } else if(monster.name == "Wither") {
+                    player.xp += 700;
+                    player.coins += 350;
+                    System.out.println("Você recebeu 700 de XP e 350 moedas por derrotar o Wither.");
+                }
+                if(player.xpMultiplier > 2) {
+                    player.xp += (float) (5 + player.xpMultiplier);
+                    System.out.println("Ganhou: " + (5 * player.xpMultiplier) + " de xp.");
+                    System.out.println("BUFF de XP: ATIVADO");
+                } else {
+                    System.out.println("Ganhou: " + 5 + " de xp.");
+                    System.out.println("BUFF de XP: DESATIVADO");
+                }
+                System.out.println("======================");
+            } else {
+                player.xp += 5;
+                System.out.println("======================");
+                if(player.haveSword == true) {
+                    System.out.println("Você usou a sua espada de esmeralda.");
+                    System.out.println("Tirou: " + player.random*2.30);
+                    monster.setHp((int) (monster.hp-player.random*2.30));
+                } else {
+                    System.out.println("Tirou: " + player.random);
+                    monster.setHp(monster.hp-player.random);
+                }
+                if(player.xpMultiplier > 2) {
+                    player.xp += (float) (5 + player.xpMultiplier);
+                    System.out.println("Ganhou: " + 5*player.xpMultiplier + " de xp.");
+                    System.out.println("BUFF de XP: ATIVADO");
+                } else {
+                    System.out.println("Ganhou: " + 5 + " de xp.");
+                    System.out.println("BUFF de XP: DESATIVADO");
+                }
+                System.out.println("Vida do " + monster.getName() + ": " + monster.hp);
+                System.out.println("======================");
+            }
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Player {
 
     String name;
+    GameCore gameCore;
     boolean haveSword = false;
     int hp = 100;
     float xp;
@@ -11,74 +12,13 @@ public class Player {
 
     int random;
 
-    public Player(String name) {
+    public Player(String name, GameCore gameCore) {
         this.name = name;
+        this.gameCore = gameCore;
     }
 
     public void attack(Monster monster) {
-        System.out.println("Você atacou a " + monster.getName());
-        if(monster.hp <= 0) {
-            System.out.println("======================");
-            System.out.println("Este monstro já está morto.");
-            System.out.println("======================");
-        } else {
-            random();
-            if(monster.hp <= 80) {
-                System.out.println("======================");
-                if(haveSword == true) {
-                    System.out.println("Você usou a sua espada de esmeralda.");
-                    System.out.println("Tirou: " + monster.hp*2.30);
-                    monster.setHp((int) (monster.hp-monster.hp*2.30));
-                } else {
-                    System.out.println("Tirou: " + monster.hp);
-                }
-                monster.setHp(monster.hp-monster.hp);
-                System.out.println("Você matou o " + monster.getName());
-                if(monster.name == "Zombie") {
-                    xp += 250;
-                    coins += 125;
-                    System.out.println("Você recebeu 250 de XP e 125 moedas por derrotar o Zombie.");
-                } else if(monster.name == "Slime") {
-                    xp += 500;
-                    coins += 250;
-                    System.out.println("Você recebeu 500 de XP e 250 moedas por derrotar a Slime.");
-                } else if(monster.name == "Wither") {
-                    xp += 700;
-                    coins += 350;
-                    System.out.println("Você recebeu 700 de XP e 350 moedas por derrotar o Wither.");
-                }
-                if(xpMultiplier > 2) {
-                    xp += (float) (5 + xpMultiplier);
-                    System.out.println("Ganhou: " + (5 * xpMultiplier) + " de xp.");
-                    System.out.println("BUFF de XP: ATIVADO");
-                } else {
-                    System.out.println("Ganhou: " + 5 + " de xp.");
-                    System.out.println("BUFF de XP: DESATIVADO");
-                }
-                System.out.println("======================");
-            } else {
-                System.out.println("======================");
-                if(haveSword == true) {
-                    System.out.println("Você usou a sua espada de esmeralda.");
-                    System.out.println("Tirou: " + random*2.30);
-                    monster.setHp((int) (monster.hp-random*2.30));
-                } else {
-                    System.out.println("Tirou: " + random);
-                    monster.setHp(monster.hp-random);
-                }
-                xp += 5;
-                if(xpMultiplier > 2) {
-                    xp += (float) (5 + xpMultiplier);
-                    System.out.println("Ganhou: " + 5*xpMultiplier + " de xp.");
-                    System.out.println("BUFF de XP: ATIVADO");
-                } else {
-                    System.out.println("Ganhou: " + 5 + " de xp.");
-                    System.out.println("BUFF de XP: DESATIVADO");
-                }
-                System.out.println("Vida do " + monster.getName() + ": " + monster.hp);
-                System.out.println("======================");
-            }
-        }
+        gameCore.checkAttack(monster);
     }
 
     public float getXp() {
@@ -87,6 +27,14 @@ public class Player {
 
     public void setHp(int hp) {
         this.hp = hp;
+    }
+
+    public void setCoins(double coins) {
+        this.coins = coins;
+    }
+
+    public void setXp(float xp) {
+        this.xp = xp;
     }
 
     public void random() {
